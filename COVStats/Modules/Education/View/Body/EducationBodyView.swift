28341bf7a0168cd8d0901f5ef262a6c4ec61c8e7
Igor Podolsiy <delusionhs@mail.ru>
2020-10-12 16:40:29 +0200

    add education body

---
+++ COVStats/Modules/Education/View/Body/EducationBodyView.swift
@@ -0,0 +1,41 @@
+//
+//  EducationBodyView.swift
+//  COVStats
+//
+//  Created by Igor Podolskiy on 12.10.2020.
+//  Copyright © 2020 Igor Podolskiy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EducationBodyView: UIView {
+
+    private enum Localization {
+    }
+
+    private enum ViewOptions {
+
+    }
+
+    private enum LayoutOptions {
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupLayouts()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+    }
+
+    private func setupLayouts() {
+
+    }
+
+}
